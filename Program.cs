b14d2bddec27e01de2668c486e0f87d7b591ce38
LeylaFedina <113465569+LeylaFedina@users.noreply.github.com>
2022-10-12 19:50:05 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,98 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+/* Задача 57: Составить частотный словарь элементов двумерного массива.
+Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+Набор данных
+Частотный массив
+{ 1, 9, 9, 0, 2, 8, 0, 9 }
+0 встречается 2 раза
+1 встречается 1 раз
+2 встречается 1 раз
+8 встречается 1 раз
+9 встречается 3 раза
+1, 2, 3
+4, 6, 1
+2, 1, 6
+1 встречается 3 раза
+2 встречается 2 раз
+3 встречается 1 раз
+4 встречается 1 раз
+6 встречается 2 раза */
+
+
+int[,] GenerateArray(int height, int weight, int deviation)
+{
+    int[,] generatedArray = new int[height, weight];
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < weight; j++)
+        {
+            generatedArray[i, j] = new Random().Next(-deviation, deviation + 1);
+        }
+    }
+    return generatedArray;
+}
+
+void printColorData(string data)
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.Write(data);
+    Console.ResetColor();
+}
+void showArray(int[,] inputArray)
+{
+    printColorData($" \t");
+    for (int i = 0; i < inputArray.GetLength(1); i++)
+    {
+        printColorData($"{i}\t");
+    }
+    Console.WriteLine();
+    for (int i = 0; i < inputArray.GetLength(0); i++)
+    {
+        printColorData($"{i}\t");
+        for (int j = 0; j < inputArray.GetLength(1); j++)
+        {
+            Console.Write($"{inputArray[i, j].ToString()}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void printArray(int[] incomingArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < incomingArray.Length; i++)
+    {
+        Console.Write(incomingArray[i]);
+        if (i < incomingArray.Length - 1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.WriteLine("]");
+}
+
+
+int[] SwopRows(int[,] inputArray)
+{
+    int count = 0;
+    int[] resultArray = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    for (int k = 0; k < resultArray.Length; k++)
+    {
+        for (int i = 0; i < inputArray.GetLength(0); i++)
+        {
+            for (int j = 0; j < inputArray.GetLength(1); j++)
+            {
+
+                if (inputArray[i, j] == resultArray[k]) ;
+                count++;
+            }
+
+        }
+        resultArray[k] = count;
+        count = 0;
+    }
+    return resultArray;
+}
+int[,] generatedArray = GenerateArray(5, 5, 10);
+showArray(generatedArray);
+int[] swoppedArray = SwopRows(generatedArray);
+printArray(swoppedArray);
